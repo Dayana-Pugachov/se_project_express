e22a7a4b89c037a69aa9bad1216496b69fbd426a
Dayana Pugachov <dayashka2000@gmail.com>
2024-05-13 12:18:14 +0300

    linter error fix

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,4 @@ const clothesRouter = require("./clothingItems");
 router.use("/users", userRouter);
 router.use("/items", clothesRouter);
 
-router.use((req, res) => {
-  if (res.status === 404) {
-    return res.send({ message: "Requested resource not found" });
-  }
-});
 module.exports = router;
