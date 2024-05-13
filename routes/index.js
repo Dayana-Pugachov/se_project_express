a4f2e90428859418d154f2e24cd5edce5ed5ebdd
Dayana Pugachov <dayashka2000@gmail.com>
2024-05-13 15:48:09 +0300

    fix/linter error in index.js

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,8 @@ const { DOCUMENT_NOT_FOUND } = require("../utils/errors");
 router.use("/users", userRouter);
 router.use("/items", clothesRouter);
 
-router.use((req, res) => {
-  return res
+router.use((req, res) => res
     .status(DOCUMENT_NOT_FOUND)
-    .send({ message: "The requested resource is not found" });
-});
+    .send({ message: "The requested resource is not found" }));
 
 module.exports = router;
