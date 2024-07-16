fb3b5c233e8aa6b74285e2d9f38dbce0148f0fde
Dayana Pugachov <dayashka2000@gmail.com>
2024-07-16 16:09:40 +0300

    update/process.env jwt

--- utils/config.js
+++ utils/config.js
@@ -1,1 +1,7 @@
-module.exports.JWT_SECRET = "my-secret-key";
+//module.exports.JWT_SECRET = "my-secret-key";
+
+const { JWT_SECRET = "super-strong-secret" } = process.env;
+
+module.exports = {
+  JWT_SECRET,
+};
